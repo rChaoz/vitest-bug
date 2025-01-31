202facaedb4ba91e89a40c1ea1e553e7c85db73f
Matei-Paul Trandafir <matei_trand@yahoo.com>
2025-01-31 17:54:21 +0200

    fix thing

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import EmailTemplate from "./template.hbs"
 import inlineCss from "inline-css"
 
 export async function generateEmail(values) {
-    const html = EmailTemplate({ ...values, baseUrl: "http://localhost:5713", contactEmail: "contact@todo-boards.com" })
+    const html = EmailTemplate({ ...values, baseUrl: "http://localhost:5173", contactEmail: "contact@todo-boards.com" })
     return inlineCss(html, { removeStyleTags: false, url: "./" })
 }
